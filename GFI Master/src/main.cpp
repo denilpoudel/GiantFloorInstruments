22894611cc49fa194f1fdab1f61b6f772e6e3f68
denilpoudel <denilpoudel@gmail.com>
2018-10-05 23:39:27 -0700

    Master Arduino LED on/off

--- GFI Master/src/main.cpp
+++ GFI Master/src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
+#include <Wire.h>
+
+#define led 8
 
 void setup() {
-    // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  Wire.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  Wire.requestFrom(8,6); // 6 bytes from address 8
+
+  if (Wire.available()){
+    char c = Wire.read();
+    delay(2);
+
+    switch (c){
+      case 'q':
+      digitalWrite(led,HIGH);
+      Serial.println("HIGH");
+      break;
+      case 'd':
+      digitalWrite(led, LOW);
+      Serial.println("LOW");
+      break;
+    }
+  }
 }
