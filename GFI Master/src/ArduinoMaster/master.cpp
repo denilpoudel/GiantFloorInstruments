ed0c2cf1f408c2b1ba7e69d517918006d3f56e78
denilpoudel <denilpoudel@gmail.com>
2018-10-16 10:52:00 -0700

    Added loops

--- GFI Master/src/ArduinoMaster/master.cpp
+++ GFI Master/src/ArduinoMaster/master.cpp
@@ -1,43 +1,46 @@
 #include <Arduino.h>
-#include <Wire.h>
+
+int ledpins[] = {1,,2,3, 4, 5, 6, 7, 8, 9}; // LED pins
+int pianobuttons[] = {10,11,12,13};
+int pianoOn[] = {};
+
+
+
 
 void setup() {
-  Wire.begin();
-  Serial.begin(9600);
+   for(int p=1; p<10; p++)
+   {
+       pinMode(ledpins[p], OUTPUT); // Set the mode to OUTPUT
+   }
+   for(int b=10; b<14; b++)
+   {
+       pinMode(pianobuttons[b], INPUT); // Set the mode to OUTPUT
+   }
 }
 
 void loop() {
-  Wire.requestFrom(2,1); // 1 byte from address 2
-
-  if (Wire.available()){
-    char c = Wire.read();
-    delay(2);
-
-    switch (c){
-      case 'c':
-      Serial.println('A');
-      break;
-      case 'd':
-      Serial.println('B');
-      break;
-      case 'e':
-      Serial.println('C');
-      break;
-      case 'f':
-      Serial.println('D');
-      break;
-      case 'g':
-      Serial.println('E');
-      break;
-      case 'a':
-      Serial.println('F');
-      break;
-      case 'b':
-      Serial.println('G');
-      break;
-      case 'v': // Should be c but cant use twice
-      Serial.println('H');
-      break;
+  for(int i = 10; i < 13; i++){
+    if (digitalRead(i) == HIGH)
+    {
+      pianoOn[i] == 1;
+    }
+    else
+    {
+      pianoOn[i] == 0;
+    }
+  }
+  for(i=10; i<13; i++)
+  {
+    for(int z=1; z<10; z++)
+      {
+        if (pianoOn[x] == 1)
+        {
+        digitalWrite(z, HIGH);
+        }
+        if(pianoOn[x] == 0)
+        {
+        digitalWrite(z, LOW);
+        }
     }
   }
 }
