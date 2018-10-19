186cdaced338493861441f6d211db6bcfc6bee60
denilpoudel <denilpoudel@gmail.com>
2018-10-18 20:52:05 -0700

    Multiple leds works

--- GFI Master/src/ArduinoMaster/master.cpp
+++ GFI Master/src/ArduinoMaster/master.cpp
@@ -1,46 +1,34 @@
 #include <Arduino.h>
 
-int ledpins[] = {1,,2,3, 4, 5, 6, 7, 8, 9}; // LED pins
-int pianobuttons[] = {10,11,12,13};
-int pianoOn[] = {};
+const int ledpin[] = {2,3,4};
+const int button[] = {9,10,11};
 
-
-
+int bval[5]={};
 
 void setup() {
-   for(int p=1; p<10; p++)
-   {
-       pinMode(ledpins[p], OUTPUT); // Set the mode to OUTPUT
-   }
-   for(int b=10; b<14; b++)
-   {
-       pinMode(pianobuttons[b], INPUT); // Set the mode to OUTPUT
-   }
+  for (int x=1;x<9;x++){
+    pinMode(ledpin[x],OUTPUT);
+  }
+  for (int b=9;b<14;b++){
+    pinMode(button[b],INPUT);
+  }
 }
 
 void loop() {
-  for(int i = 10; i < 13; i++){
-    if (digitalRead(i) == HIGH)
+
+  bval[0] = digitalRead(button[0]);
+  bval[1] = digitalRead(button[1]);
+  bval[2] = digitalRead(button[2]);
+  bval[3] = digitalRead(button[3]);
+
+  for (int x=0;x<4;x++)
+  {
+    if(bval[x] == HIGH)
     {
-      pianoOn[i] == 1;
+      digitalWrite(ledpin[x],HIGH);
     }
     else
-    {
-      pianoOn[i] == 0;
-    }
+      digitalWrite(ledpin[x],LOW);
   }
-  for(i=10; i<13; i++)
-  {
-    for(int z=1; z<10; z++)
-      {
-        if (pianoOn[x] == 1)
-        {
-        digitalWrite(z, HIGH);
-        }
-        if(pianoOn[x] == 0)
-        {
-        digitalWrite(z, LOW);
-        }
-    }
-  }
+
 }
